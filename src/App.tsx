12a382647c550eb7ae364d44665caf9e3519bcdd
Vitalii Tkachenko <vitozov9@gmail.com>
2023-06-22 12:31:16 -0600

    add social media project to git

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Hello, samurai! Let's go!
+
+     Hello, Dimych
+    </div>
+  );
+}
+
+export default App;
